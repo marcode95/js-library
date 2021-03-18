a6d89bb6d438d5cb74c3f5db9743118974bf197c
taiwo2 <adetonat@ymail.com>
2021-03-18 15:13:38 +0100

    Add changes

--- scripts/addBook.js
+++ scripts/addBook.js
@@ -1,16 +1,55 @@
-function addBookToLibrary() {
-  let title = window.prompt("Enter title: ");
-  let author = window.prompt("Enter author: ");
-  let pages = window.prompt("Enter pages: ");
-  let answer = window.confirm("Press OK if you already read this book! Cancel if you didn't!");
-    if (answer) {
-      read = true;
-    }
-    else {
-      read = false;
-    }
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
+const formToggler = document.getElementById('formToggler');
+const form = document.getElementById('form');
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const pagesInput = document.getElementById('pagesInput');
+const checkboxInput = document.getElementById('readCheckbox');
+const bookList = document.getElementById('bookList');
+
+let myBooks = [];
+
+function Book(title = '', author = '', pages = '', read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function createBookCard(book) {
+  const bookCard = document.createElement('div');
+  bookCard.setAttribute('data-title', book.title);
+
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h3');
+  const bookPages = document.createElement('p');
+  const readStatus = document.createElement('p');
+
+  const removeButton = document.createElement('button');
+  removeButton.setAttribute('type', 'button');
+  removeButton.innerText = 'Remove book';
+
+  const readButton = document.createElement('button');
+  readButton.setAttribute('type', 'button');
+  readButton.innerText = book.read ? 'Unread' : 'Read';
+
+  bookCard.classList.add('book-card');
+  bookTitle.innerText = `Title: ${book.title}`;
+  bookAuthor.innerText = `Author: ${book.author}`;
+  bookPages.innerText = `Pages: ${book.pages}`;
+  readStatus.innerText = `Read: ${book.read}`;
+
+  bookCard.append(bookTitle);
+  bookCard.append(bookAuthor);
+  bookCard.append(bookPages);
+  bookCard.append(readStatus);
+  bookCard.append(removeButton);
+  bookCard.append(readButton);
+  bookList.append(bookCard);
+
+  /* eslint-disable no-use-before-define */
+  removeButton.addEventListener('click', removeBook);
+  readButton.addEventListener('click', toggleReadStatus);
+  /* eslint-enable no-use-before-define */
 }
 
 addBookToLibrary();
