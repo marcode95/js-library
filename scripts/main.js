c1f876fab655cc7bfbe2d8448ff3ad6e139ee901
Marco <marcoerhardt95@gmail.com>
2021-03-19 13:52:00 +0100

    Fix all linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,94 @@
-let myLibrary = [];
-
+const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   if (read) {
     this.read = 'already read';
-    }
-  else {
+  } else {
     this.read = 'not yet read';
-  }
-  info = function() {
-    return(title + ' by ' + author + ', ' + pages + ' pages, ' + readyet);
   }
 }
 
+function createBookCard() {
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const formCard = document.createElement('div');
+    formCard.setAttribute('data-id', i);
 
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const bookRead = document.createElement('p');
 
+    bookTitle.textContent = myLibrary[i].title;
+    bookAuthor.textContent = myLibrary[i].author;
+    bookPages.textContent = myLibrary[i].pages;
+    bookRead.textContent = myLibrary[i].read;
+
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('type', 'button');
+    removeButton.setAttribute('id', 'removebutton');
+    removeButton.setAttribute('data-id', i);
+    removeButton.innerText = 'Remove book';
+
+    const readButton = document.createElement('button');
+    readButton.setAttribute('type', 'button');
+    readButton.setAttribute('id', 'readbutton');
+    readButton.setAttribute('data-id', i);
+    readButton.innerText = 'Read?';
+
+    const booksContainer = document.querySelector('#books-container');
+    booksContainer.appendChild(formCard);
+    formCard.appendChild(bookTitle);
+    formCard.appendChild(bookAuthor);
+    formCard.appendChild(bookPages);
+    formCard.appendChild(bookRead);
+    formCard.appendChild(removeButton);
+    formCard.appendChild(readButton);
+  }
+}
+
+function emptyContainer() {
+  const booksContainer = document.querySelector('#books-container');
+  booksContainer.innerHTML = '';
+}
+// eslint-disable-next-line no-unused-vars
+function addBookToLibrary() {
+  const title = document.getElementById('titleInput').value;
+  const author = document.getElementById('authorInput').value;
+  const pages = document.getElementById('pagesInput').value;
+  const read = document.getElementById('readCheckbox').checked;
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  emptyContainer();
+  createBookCard();
+}
+
+function removeBook(id) {
+  myLibrary.splice(id, 1);
+}
+
+const booksContainer = document.querySelector('#books-container');
+booksContainer.addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'removebutton') {
+    removeBook(e.target.dataset.id);
+    emptyContainer();
+    createBookCard();
+  }
+});
+
+function readCheck(id) {
+  if (myLibrary[id].read === 'already read') {
+    myLibrary[id].read = 'not yet read';
+  } else {
+    myLibrary[id].read = 'already read';
+  }
+}
+
+booksContainer.addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'readbutton') {
+    readCheck(e.target.dataset.id);
+    emptyContainer();
+    createBookCard();
+  }
+});
